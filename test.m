76ce5acbcb7bafcdd89ff4891f0944eb18ed0e3c
dunnywei <wei_don@yahoo.com>
2018-04-25 19:31:11 -0400

    Lecture_30
    https://www.youtube.com/watch?v=0TYu9dv7uDA&index=30&list=PLAwxTw4SYaPnbDacyrK_kB_RUkuxQBlCm

--- test.m
+++ test.m
@@ -1,5 +1,18 @@
+%Lecture 30_crop an image
+img=imread('bycycle.png');
+imshow(img);
+disp(size(img));
+
+cropped=img(110:310,10:160);
+imshow(cropped);
+
+
+
+%{
 %at a given location (row,col);
 img=imrad(‘abc.png’);
+imshow(img);
 disp(size(img));
 img(50,100);%row 50 and col 100
 Relative the high value waves it is.others are comparative low
+%}
