5fcd6d0ed9846736448aad9eb953377b238e75f4
dunnywei <wei_don@yahoo.com>
2018-05-09 21:24:49 -0400

    lecture 31 done

--- test.m
+++ test.m
@@ -1,11 +1,26 @@
+%lecture 31_color_planes
+%https://www.youtube.com/watch?v=dqTtAcljH_8&list=PLAwxTw4SYaPnbDacyrK_kB_RUkuxQBlCm&index=30
+%{
+img=imread('fruit.png')
+imshow(img);
+disp(size(img));
+im_red=img(:,:,1);
+imshow(im_red); 
+%0:46->bright area shows high red value, darker areas are vice versa
+
+plot(img_red(150,0))
+%}
+
+
 %Lecture 30_crop an image
+%{
 img=imread('bycycle.png');
 imshow(img);
 disp(size(img));
 
 cropped=img(110:310,10:160);
 imshow(cropped);
-
+%}
 
 
 %{
